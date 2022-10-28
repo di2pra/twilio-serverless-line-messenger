9e14c00a541740208b3f913cc93b8885052be732
Pradheep Rajendirane <pas495@gmail.com>
2022-10-28 10:54:34 +0200

    add support for image

--- src/functions/inbound.ts
+++ src/functions/inbound.ts
@@ -1,5 +1,9 @@
+import { Client as LineClient } from '@line/bot-sdk';
 import '@twilio-labs/serverless-runtime-types';
 import { Context, ServerlessCallback, ServerlessFunctionSignature } from '@twilio-labs/serverless-runtime-types/types';
+import jose from 'node-jose';
+import { DocumentInstance, DocumentListInstance } from 'twilio/lib/rest/sync/v1/service/document';
+const fetch = require('node-fetch');
 
 type MyEvent = {
   events?: {
@@ -21,8 +25,87 @@ type MyContext = {
   ACCOUNT_SID: string;
   AUTH_TOKEN: string;
   FLEX_CONVERSATION_SERVICE_SID: string;
+  CUSTOM_DOMAIN_NAME: string;
   DOMAIN_NAME: string;
+  TWILIO_SYNC_SERVICE_SID: string;
+  LINE_CHANNEL_SECRET: string;
+  LINE_ASSERTION_SIGNING_KEY: string;
   FLEX_STUDIO_FLOW_SID: string;
+  SERVICE_SID: string;
+  LINE_CHANNEL_ID: string;
+}
+
+type CAToken = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  key_id: string;
+}
+
+// Helper method to simplify getting a Sync resource (Document, List, or Map)
+// that handles the case where it may not exist yet.
+const getOrCreateResource: (resource: DocumentListInstance, name: string, options?: object) => Promise<DocumentInstance> = async (resource, name, options = {}) => {
+  try {
+    // Does this resource (Sync Document, List, or Map) exist already? Return it
+    return await resource(name).fetch();
+  } catch (err) {
+    // It doesn't exist, create a new one with the given name and return it
+    // @ts-ignore
+    options.uniqueName = name;
+    return await resource.create(options);
+  }
+};
+
+const getLineChannelAccessToken: ({ syncServiceSid, privateKey, assertionSigningKey, channelId }: { syncServiceSid: string, privateKey: string, assertionSigningKey: string, channelId: string }) => Promise<CAToken> = async ({ syncServiceSid, privateKey, assertionSigningKey, channelId }) => {
+
+  const syncClient = Runtime.getSync({ serviceName: syncServiceSid });
+
+  const document = await getOrCreateResource(syncClient.documents, 'line-channel-access-token');
+
+  let CAToken: CAToken = document.data;
+
+  if (CAToken.access_token === undefined) {
+
+    const header = {
+      alg: "RS256",
+      typ: "JWT",
+      kid: assertionSigningKey
+    };
+
+    const payload = {
+      iss: channelId,
+      sub: channelId,
+      aud: "https://api.line.me/",
+      exp: Math.floor(new Date().getTime() / 1000) + 60 * 30,
+      token_exp: 60 * 60 * 24 * 30
+    };
+
+    const JWToken = await jose.JWS.createSign({ format: 'compact', fields: header }, JSON.parse(privateKey))
+      .update(JSON.stringify(payload))
+      .final();
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
+    params.append('client_assertion_type', 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer');
+    params.append('client_assertion', JWToken.toString());
+
+    const request = await fetch(`https://api.line.me/oauth2/v2.1/token`, {
+      method: "POST",
+      body: params
+    });
+
+    if (!request.ok) {
+      throw new Error('Error while retrieving channel access token from Line');
+    }
+
+    CAToken = request.json() as CAToken;
+
+    await document.update({ data: CAToken, ttl: 60 * 60 * 24 * 29 });
+
+  }
+
+  return CAToken;
+
 }
 
 export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async function (
@@ -37,6 +120,7 @@ export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async fu
       throw new Error('No Message')
     }
 
+    // retrieve incoming message infomation
     const incomingMessage = event.events.find(e => e.type === "message");
 
     if (!incomingMessage) {
@@ -49,13 +133,30 @@ export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async fu
     const conversationList = await twilioClient.conversations.v1.services(context.FLEX_CONVERSATION_SERVICE_SID).participantConversations.list({ identity: lineIdentity });
 
     const existingConversation = conversationList.find(conversation => conversation.conversationState !== 'closed');
+    let conversationSid = existingConversation?.conversationSid;
 
-    if (!existingConversation) {
+    // init Line Client
+    const syncServiceSid = context.TWILIO_SYNC_SERVICE_SID || 'default';
+    const assertionSigningKey = context.LINE_ASSERTION_SIGNING_KEY;
+    const channelId = context.LINE_CHANNEL_ID;
+    const privateKey = Runtime.getAssets()['/private_key.key'].open();
+    const CAToken: CAToken = await getLineChannelAccessToken({ syncServiceSid: syncServiceSid, privateKey: privateKey, assertionSigningKey: assertionSigningKey, channelId: channelId });
+
+    const lineClient = new LineClient({
+      channelAccessToken: CAToken.access_token,
+      channelSecret: context.LINE_CHANNEL_SECRET
+    });
+
+    // Retrieve Line User Profile
+    const userProfile = await lineClient.getProfile(incomingMessage.source.userId);
+
+    if (!conversationSid) {
 
       const conversationCreated = await twilioClient.conversations.v1.conversations.create({
         friendlyName: `Line Conversation`,
         attributes: JSON.stringify({
-          replyToken: incomingMessage.replyToken
+          userId: incomingMessage.source.userId,
+          name: userProfile.displayName
         })
       });
 
@@ -80,36 +181,66 @@ export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async fu
           configuration: {
             filters: 'onMessageAdded',
             method: 'POST',
-            url: `https://${context.DOMAIN_NAME}/outbound`
+            url: `https://${context.SERVICE_SID ? context.DOMAIN_NAME : context.CUSTOM_DOMAIN_NAME}/outbound`
           }
         });
 
-      // add Line User Message to the Conversation
-      await twilioClient.conversations.v1.conversations(conversationCreated.sid)
-        .messages
-        .create({
-          author: lineIdentity, body: incomingMessage.message.text,
-          xTwilioWebhookEnabled: 'true'
+      conversationSid = conversationCreated.sid;
+
+    }
+
+    switch (incomingMessage.message.type) {
+      case "text":
+        // add Line User Text Message to the Conversation
+        await twilioClient.conversations.v1.conversations(conversationSid)
+          .messages
+          .create({
+            author: userProfile.displayName,
+            body: incomingMessage.message.text,
+            xTwilioWebhookEnabled: 'true'
+          });
+
+        break;
+      case "image":
+
+        const getContentRequest = await fetch(`https://api-data.line.me/v2/bot/message/${incomingMessage.message.id}/content`, {
+          method: "GET",
+          headers: {
+            'Authorization': `Bearer ${CAToken.access_token}`
+          }
         });
 
-    } else {
+        if (!getContentRequest.ok) {
+          throw new Error('Error while retrieving Image Content');
+        }
 
-      // add Line User Message to the Conversation
-      await twilioClient.conversations.v1.conversations(existingConversation.conversationSid)
-        .messages
-        .create({
-          author: lineIdentity,
-          body: incomingMessage.message.text,
-          xTwilioWebhookEnabled: 'true'
+        const uploadContentRequest = await fetch(`https://mcs.us1.twilio.com/v1/Services/${context.FLEX_CONVERSATION_SERVICE_SID}/Media`, {
+          method: "POST",
+          headers: {
+            'Authorization': `Basic ${Buffer.from(`${context.ACCOUNT_SID}:${context.AUTH_TOKEN}`).toString('base64')}`
+          },
+          body: await getContentRequest.blob()
         });
 
-      const existingAttribute = JSON.parse(existingConversation.conversationAttributes);
+        if (!uploadContentRequest.ok) {
+          throw new Error('Error while upload the content to twilio');
+        }
 
-      await twilioClient.conversations.v1.conversations(existingConversation.conversationSid)
-        .update({
-          attributes: JSON.stringify({ ...existingAttribute, replyToken: incomingMessage.replyToken })
-        })
+        const responseUploadContent = await uploadContentRequest.json();
 
+        // add Line User Text Message to the Conversation
+        await twilioClient.conversations.v1.conversations(conversationSid)
+          .messages
+          .create({
+            author: userProfile.displayName,
+            mediaSid: responseUploadContent.sid,
+            xTwilioWebhookEnabled: 'true'
+          });
+
+
+        break;
+      default:
+        break;
     }
 
     // Return a success response using the callback function.
@@ -123,7 +254,6 @@ export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async fu
     } else {
       return callback(null, new Error('Unknown Error'));
     }
-
 
   }
 };
--- src/functions/outbound.ts
+++ src/functions/outbound.ts
@@ -1,19 +1,98 @@
 import { Client as LineClient } from '@line/bot-sdk';
 import '@twilio-labs/serverless-runtime-types';
 import { Context, ServerlessCallback, ServerlessFunctionSignature } from '@twilio-labs/serverless-runtime-types/types';
+import jose from 'node-jose';
+import { DocumentInstance, DocumentListInstance } from 'twilio/lib/rest/sync/v1/service/document';
+const fetch = require('node-fetch');
 
 type MyEvent = {
   Source: string;
   ConversationSid: string;
   Body: string;
+  Media?: string;
 }
 
 type MyContext = {
   ACCOUNT_SID: string;
   AUTH_TOKEN: string;
   FLEX_CONVERSATION_SERVICE_SID: string;
-  LINE_CHANNEL_ACCESS_TOKEN: string;
+  LINE_CHANNEL_ID: string;
   LINE_CHANNEL_SECRET: string;
+  LINE_ASSERTION_SIGNING_KEY: string;
+  TWILIO_SYNC_SERVICE_SID: string;
+}
+
+type CAToken = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  key_id: string;
+}
+
+// Helper method to simplify getting a Sync resource (Document, List, or Map)
+// that handles the case where it may not exist yet.
+const getOrCreateResource: (resource: DocumentListInstance, name: string, options?: object) => Promise<DocumentInstance> = async (resource, name, options = {}) => {
+  try {
+    // Does this resource (Sync Document, List, or Map) exist already? Return it
+    return await resource(name).fetch();
+  } catch (err) {
+    // It doesn't exist, create a new one with the given name and return it
+    // @ts-ignore
+    options.uniqueName = name;
+    return await resource.create(options);
+  }
+};
+
+const getLineChannelAccessToken: ({ syncServiceSid, privateKey, assertionSigningKey, channelId }: { syncServiceSid: string, privateKey: string, assertionSigningKey: string, channelId: string }) => Promise<CAToken> = async ({ syncServiceSid, privateKey, assertionSigningKey, channelId }) => {
+
+  const syncClient = Runtime.getSync({ serviceName: syncServiceSid });
+
+  const document = await getOrCreateResource(syncClient.documents, 'line-channel-access-token');
+
+  let CAToken: CAToken = document.data;
+
+  if (CAToken.access_token === undefined) {
+
+    const header = {
+      alg: "RS256",
+      typ: "JWT",
+      kid: assertionSigningKey
+    };
+
+    const payload = {
+      iss: channelId,
+      sub: channelId,
+      aud: "https://api.line.me/",
+      exp: Math.floor(new Date().getTime() / 1000) + 60 * 30,
+      token_exp: 60 * 60 * 24 * 30
+    };
+
+    const JWToken = await jose.JWS.createSign({ format: 'compact', fields: header }, JSON.parse(privateKey))
+      .update(JSON.stringify(payload))
+      .final();
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'client_credentials');
+    params.append('client_assertion_type', 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer');
+    params.append('client_assertion', JWToken.toString());
+
+    const request = await fetch(`https://api.line.me/oauth2/v2.1/token`, {
+      method: "POST",
+      body: params
+    });
+
+    if (!request.ok) {
+      throw new Error('Error while retrieving channel access token from Line');
+    }
+
+    CAToken = request.json() as CAToken;
+
+    await document.update({ data: CAToken, ttl: 60 * 60 * 24 * 29 });
+
+  }
+
+  return CAToken;
+
 }
 
 export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async function (
@@ -31,9 +110,16 @@ export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async fu
 
     const twilioClient = context.getTwilioClient();
 
+    const syncServiceSid = context.TWILIO_SYNC_SERVICE_SID || 'default';
+    const assertionSigningKey = context.LINE_ASSERTION_SIGNING_KEY;
+    const channelId = context.LINE_CHANNEL_ID;
+    const privateKey = Runtime.getAssets()['/private_key.key'].open();
+
+    const CAToken: CAToken = await getLineChannelAccessToken({ syncServiceSid: syncServiceSid, privateKey: privateKey, assertionSigningKey: assertionSigningKey, channelId: channelId });
+
     // create LINE SDK client
     const lineClient = new LineClient({
-      channelAccessToken: context.LINE_CHANNEL_ACCESS_TOKEN,
+      channelAccessToken: CAToken.access_token,
       channelSecret: context.LINE_CHANNEL_SECRET
     });
 
@@ -41,10 +127,52 @@ export const handler: ServerlessFunctionSignature<MyContext, MyEvent> = async fu
 
     const attributes = JSON.parse(conversation.attributes);
 
-    const response = await lineClient.replyMessage(
-      attributes.replyToken,
-      { type: 'text', text: event.Body }
-    );
+    if (event.Body) {
+      await lineClient.pushMessage(
+        attributes.userId,
+        { type: 'text', text: event.Body }
+      );
+    }
+
+    if (event.Media) {
+
+      const mediaObject = JSON.parse(event.Media) as {
+        Sid: string;
+        Filename: string;
+        Size: number
+      }[];
+
+      if (mediaObject.length > 0) {
+
+        let lineMessageList = [] as {
+          type: "image";
+          originalContentUrl: string;
+          previewImageUrl: string;
+        }[];
+
+        for await (const mediaItem of mediaObject) {
+          const retrieveMediaUrl = await fetch(`https://mcs.us1.twilio.com/v1/Services/${context.FLEX_CONVERSATION_SERVICE_SID}/Media/${mediaItem.Sid}`, {
+            method: "GET",
+            headers: {
+              'Authorization': `Basic ${Buffer.from(`${context.ACCOUNT_SID}:${context.AUTH_TOKEN}`).toString('base64')}`
+            }
+          });
+
+          const responseMedia = await retrieveMediaUrl.json();
+
+          lineMessageList.push({ type: 'image', originalContentUrl: responseMedia.links.content_direct_temporary, previewImageUrl: responseMedia.links.content_direct_temporary })
+
+        }
+
+        await lineClient.pushMessage(
+          attributes.userId,
+          lineMessageList
+        );
+
+      }
+
+    }
+
 
     // Return a success response using the callback function.
     return callback(null);
